60561b237f8650e3d058c4dbec9d53ca3903f1e8
MJ-check <mj188163@163.com>
2020-05-24 23:50:28 +0800

    a game

---
+++ game/precache-manifest.9c51510ff0690191783177ed39a99f83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1bf3014ea292c99a0c021110b418d5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2773b8df46df8aecaec0",
+    "url": "./static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "a740ed8f3265a1da9ecc",
+    "url": "./static/js/2.af71590a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.af71590a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2773b8df46df8aecaec0",
+    "url": "./static/js/main.0d4c85db.chunk.js"
+  },
+  {
+    "revision": "fe492bec4e044aabfe13",
+    "url": "./static/js/runtime-main.4f92633c.js"
+  }
+]);
